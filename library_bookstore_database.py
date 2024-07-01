647f8bccc7c9724cb11310f226447eb0aa1941f9
Ramy Yousif <ramy_yousif@hotmail.fr>
2024-07-01 21:11:43 +0100

    Update library_bookstore_database.py

--- library_bookstore_database.py
+++ library_bookstore_database.py
@@ -2,7 +2,7 @@
 
 # I created this program for a bookstore, where a bookstore cleric or a  user can add new books, update 
 # books, delete books or simply search for available books by using the book ID, book title or book author.
-# This is then saved and updated on a database using SQL.
+
 
 
 # Importing sqlite3.
